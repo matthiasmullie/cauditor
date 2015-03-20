b3e51da031510edd931fd91c7fb2ccc88b9c80fb
Matthias Mullie <git@mullie.eu>
2015-03-20 12:18:53 +0100

    Move graph types out of code, into config

--- controllers/fallback.py
+++ controllers/fallback.py
@@ -9,59 +9,8 @@ class Controller(object):
         return re.match("", self.uri)
 
     def args(self):
-        return {
-            # @todo: get this elsewhere; don't want this huge dict in this controller
-            "graphs": [
-                {
-                    'code': "ccn",
-                    'name': "Cyclomatic Complexity",
-                    'file': "/assets/js/visualizations/treemap/CyclomaticComplexity.js",
-                    'class': "Codegraphs.Visualization.Treemap.CyclomaticComplexity",
-                },
-                {
-                    'code': "npath",
-                    'name': "Npath Complexity",
-                    'file': "/assets/js/visualizations/treemap/NpathComplexity.js",
-                    'class': "Codegraphs.Visualization.Treemap.NpathComplexity",
-                },
-                {
-                    'code': "wmc",
-                    'name': "Weighted Metric Count",
-                    'file': "/assets/js/visualizations/treemap/WeightedMetricCount.js",
-                    'class': "Codegraphs.Visualization.Treemap.WeightedMetricCount",
-                },
-                {
-                    'code': "dit",
-                    'name': "Depth Inheritance Tree",
-                    'file': "/assets/js/visualizations/treemap/DepthInheritanceTree.js",
-                    'class': "Codegraphs.Visualization.Treemap.DepthInheritanceTree",
-                },
-                {
-                    'code': "cr",
-                    'name': "Code Rank",
-                    'file': "/assets/js/visualizations/treemap/CodeRank.js",
-                    'class': "Codegraphs.Visualization.Treemap.CodeRank",
-                },
-                {
-                    'code': "ca",
-                    'name': "Afferent Coupling",
-                    'file': "/assets/js/visualizations/treemap/AfferentCoupling.js",
-                    'class': "Codegraphs.Visualization.Treemap.AfferentCoupling",
-                },
-                {
-                    'code': "ce",
-                    'name': "Efferent Coupling",
-                    'file': "/assets/js/visualizations/treemap/EfferentCoupling.js",
-                    'class': "Codegraphs.Visualization.Treemap.EfferentCoupling",
-                },
-                {
-                    'code': "i",
-                    'name': "Instability",
-                    'file': "/assets/js/visualizations/treemap/Instability.js",
-                    'class': "Codegraphs.Visualization.Treemap.Instability",
-                },
-            ],
-        }
+        import container
+        return container.load_config()
 
     def render(self):
         from jinja2 import Environment, FileSystemLoader
