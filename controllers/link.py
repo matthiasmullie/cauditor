a13fdc26bebe52f439c641691f9ed5d7f069e29a
Matthias Mullie <git@mullie.eu>
2015-04-30 09:13:55 +0200

    Add importer jobs

--- controllers/link.py
+++ controllers/link.py
@@ -36,6 +36,7 @@ class Controller(fallback.Controller):
 
     def process(self, repo, action):
         import models
+        import importers
 
         model = models.projects.Projects()
         repo = self.get_repo(repo)
@@ -49,6 +50,11 @@ class Controller(fallback.Controller):
                 'github_hook': hook.id,
             }
             model.store(project)
+
+            # create importer jobs
+            jobs = models.jobs.Jobs()
+            jobs.add(importers.last_commit.Importer(project['name']))
+            jobs.add(importers.from_first_commit.Importer(project['name']))
         else:  # unlink
             results = model.select(name=repo.full_name)
             project = next(results)
--- controllers/webhook.py
+++ controllers/webhook.py
@@ -1,1 +1,33 @@
-# @todo this is github's webhook on push & pr
+from controllers import fallback
+import os
+import sys
+import json
+import models
+import importers
+
+
+class Controller(fallback.Controller):
+    template = ""
+
+    # hook is triggered for these events:
+    # https://developer.github.com/v3/activity/events/types/#pushevent
+    # https://developer.github.com/v3/activity/events/types/#pullrequestevent
+    def __init__(self):
+        super(Controller, self).__init__()
+
+        length = int(os.environ['CONTENT_LENGTH'])
+        data = sys.stdin.read(length)
+        payload = json.loads(data)
+
+        # can't examine X-Github-Event header...
+        # pull_request doesn't have the 'ref' we're checking for, so that's how we can ignore that
+        if not 'ref' in payload:
+            return
+
+        # only care about commits to master, no support for other branches (yet)
+        if payload['ref'] != "refs/heads/master":
+            return
+
+        # create importer job
+        jobs = models.jobs.Jobs()
+        jobs.add(importers.since_last_commit.Importer(payload['repository']['full_name']))
