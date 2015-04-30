22d313a8162a7820d05c9c0639d9580b8c994d42
Matthias Mullie <git@mullie.eu>
2015-04-30 09:13:46 +0200

    Let importers extend from 'job'

--- importers/from_first_commit.py
+++ importers/from_first_commit.py
@@ -4,7 +4,7 @@ import container
 import models
 
 
-class Importer(object):
+class Importer(models.jobs.Job):
     list_commits_command = "git log --pretty=format:'%H\t%ae\t%ad\t%ce\t%cd' --reverse --date=local {hash}"
     includes_skip = False  # skips first item returned by list_commits_command, because it's "previous" to compare with
 
