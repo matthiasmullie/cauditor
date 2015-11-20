461a2efcda6289abf55e8affd11cbd662e438255
Matthias Mullie <git@mullie.eu>
2015-11-20 13:03:30 +0100

    Consolidate cgi & wsgi into same file

--- application.py
+++ application.py
@@ -5,6 +5,7 @@ import os
 sys.path.append(os.path.dirname(__file__))
 os.chdir(os.path.dirname(__file__))
 
+
 def application(environ, start_response):
     import controllers
 
@@ -17,3 +18,15 @@ def application(environ, start_response):
 
     start_response(controller.status, headers + cookies + [('Content-Length', str(len(body)))])
     return [body]
+
+
+if __name__ == '__main__':
+    # this will wrap around above mod_wsgi-compatible code to
+    # respond to cgi-script requests
+    def start_response(status, headers):
+        print("Status: " + status)
+        print("\n".join([key + ": " + value for key, value in headers]))
+        print()
+
+    body = application(os.environ, start_response)[0]
+    print(body.decode('utf-8'))
