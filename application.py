a2fcb4993fe65ddfe2a592d18e92a32eb00f81b7
Matthias Mullie <git@mullie.eu>
2015-11-25 22:03:31 +0100

    Improve current path

--- application.py
+++ application.py
@@ -4,8 +4,8 @@ import sys
 import os
 
 # add current director to ensure these modules can be imported
-sys.path.append(os.path.dirname(__file__))
-os.chdir(os.path.dirname(__file__))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def application(environ, start_response):
