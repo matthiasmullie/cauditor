a27a1118e208b46e4e8d8f2d120b9d3531df999b
Matthias Mullie <git@mullie.eu>
2015-11-25 20:43:52 +0100

    Add AWS import storage

--- listeners/__init__.py
+++ listeners/__init__.py
@@ -1,9 +1,14 @@
-from listeners import store_filesystem, store_db
+from listeners import store_db, store_aws, store_filesystem
+import container
 
 
 def execute(project, branch, commit, data, previous):
     # @todo this is really poor!
-    # @todo I guess the listeners should be configurable? (config.yaml)
-    # @todo store_aws
-    store_filesystem.execute(project, branch, commit, data, previous)
+
+    config = container.load_config()
+    if config['s3']['bucket']:
+        store_aws.execute(project, branch, commit, data, previous)
+    else:
+        store_filesystem.execute(project, branch, commit, data, previous)
+
     store_db.execute(project, branch, commit, data, previous)
