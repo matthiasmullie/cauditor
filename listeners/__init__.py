ca4fa3bba6d5f0a278527e22570bdd0af9574532
Matthias Mullie <git@mullie.eu>
2015-11-25 22:04:00 +0100

    Fix S3 config detection

--- listeners/__init__.py
+++ listeners/__init__.py
@@ -6,7 +6,7 @@ def execute(project, branch, commit, data, previous):
     # @todo this is really poor!
 
     config = container.load_config()
-    if config['s3']['bucket']:
+    if config['s3']['bucket'] != "None":
         store_aws.execute(project, branch, commit, data, previous)
     else:
         store_filesystem.execute(project, branch, commit, data, previous)
