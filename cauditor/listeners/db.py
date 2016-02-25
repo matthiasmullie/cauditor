af868f9f270359a233ca98260457955c572d60ff
Matthias Mullie <git@mullie.eu>
2016-02-25 14:39:17 +0100

    Metrics can be None, but that's not allowd - should be 0

--- cauditor/listeners/db.py
+++ cauditor/listeners/db.py
@@ -26,17 +26,17 @@ def store_commit(commit, metrics):
     commits = models.commits.Commits()
     commit.update({
         # metrics
-        'loc': metrics['loc'] if 'loc' in metrics else 0,
-        'noc': metrics['noc'] if 'noc' in metrics else 0,
-        'nom': metrics['nom'] if 'nom' in metrics else 0,
-        'ca': metrics['ca'] if 'ca' in metrics else 0,
-        'ce': metrics['ce'] if 'ce' in metrics else 0,
-        'i': metrics['i'] if 'i' in metrics else 0,
-        'dit': metrics['dit'] if 'dit' in metrics else 0,
-        'ccn': metrics['ccn'] if 'ccn' in metrics else 0,
-        'npath': metrics['npath'] if 'npath' in metrics else 0,
-        'he': metrics['he'] if 'he' in metrics else 0,
-        'hi': metrics['hi'] if 'hi' in metrics else 0,
-        'mi': metrics['mi'] if 'mi' in metrics else 0,
+        'loc': metrics['loc'] or 0 if 'loc' in metrics else 0,
+        'noc': metrics['noc'] or 0 if 'noc' in metrics else 0,
+        'nom': metrics['nom'] or 0 if 'nom' in metrics else 0,
+        'ca': metrics['ca'] or 0 if 'ca' in metrics else 0,
+        'ce': metrics['ce'] or 0 if 'ce' in metrics else 0,
+        'i': metrics['i'] or 0 if 'i' in metrics else 0,
+        'dit': metrics['dit'] or 0 if 'dit' in metrics else 0,
+        'ccn': metrics['ccn'] or 0 if 'ccn' in metrics else 0,
+        'npath': metrics['npath'] or 0 if 'npath' in metrics else 0,
+        'he': metrics['he'] or 0 if 'he' in metrics else 0,
+        'hi': metrics['hi'] or 0 if 'hi' in metrics else 0,
+        'mi': metrics['mi'] or 0 if 'mi' in metrics else 0,
     })
     commits.store(commit)
