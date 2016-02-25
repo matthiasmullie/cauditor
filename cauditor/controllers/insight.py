f4847f3418401f7fcbdade8e1f7e50a9b75db7ab
Matthias Mullie <git@mullie.eu>
2016-02-25 17:57:47 +0100

    Adjust insights to new schema

--- cauditor/controllers/insight.py
+++ cauditor/controllers/insight.py
@@ -8,13 +8,38 @@ class Controller(fallback.Controller):
     def __init__(self):
         super(Controller, self).__init__()
 
-        self.commits = [i for i in self.load_commits()] if self.settings else {}
+        # filter out commits for which we can't find a previous commit, which could be:
+        # - part of a push where not all commits were tested individually
+        # - tree changes were force-pushed after parts had already been analyzed
+        commits = [commit for commit in self.load_commits()] if self.settings else {}
+        prev_commits = {commit['hash']: commit for commit in self.load_prev_commits(commits)}
+        count = len(commits)
+
+        self.commits = [commit for commit in commits if commit['previous'] is None or commit['previous'] in prev_commits]
+        self.missing = 1 - (len(self.commits) / count)
+
+        missing_projects = set([commit['project'] for commit in commits if commit['previous'] is not None and commit['previous'] not in prev_commits])
+        self.missing_projects = [project for project in self.load_projects(missing_projects)]
+
+        # calculate differences in metrics between current & previous commits
+        charts = self.config()['charts']
+        for commit in self.commits:
+            metrics = {}
+            for chart in charts:
+                if commit['previous'] is None:
+                    metrics.update({chart['code']: float(commit[chart['code']])})
+                else:
+                    metrics.update({chart['code']: float(commit[chart['code']] - prev_commits[commit['previous']][chart['code']])})
+
+            commit.update({'diff': metrics})
 
     def args(self):
         args = super(Controller, self).args()
 
         args.update({
             'commits': self.commits,
+            'missing': self.missing,
+            'missing_projects': self.missing_projects,
             'title': args['user']['name'] if args['user'] else ''
         })
         return args
@@ -23,3 +48,12 @@ class Controller(fallback.Controller):
         model = models.commits.Commits()
         emails = self.settings['emails'].split(',')
         return model.select(author=emails, options=["ORDER BY timestamp DESC", "LIMIT 5000"]) if emails else {}
+
+    def load_prev_commits(self, commits):
+        model = models.commits.Commits()
+        hashes = [commit['previous'] for commit in commits]
+        return model.select(hash=hashes)
+
+    def load_projects(self, projects):
+        model = models.projects.Projects()
+        return model.select(name=projects)
