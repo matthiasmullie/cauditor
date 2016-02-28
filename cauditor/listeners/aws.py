e4333b8cc3e1f62ccc7d64e301010079fafa15af
Matthias Mullie <git@mullie.eu>
2016-02-28 21:02:25 +0100

    Fix S3 upload

--- cauditor/listeners/aws.py
+++ cauditor/listeners/aws.py
@@ -7,14 +7,19 @@ from cauditor import container
 def execute(project, commit, metrics):
     config = container.load_config()
 
-    s3 = boto3.resource(
+    client = boto3.client(
         service_name='s3',
+        region_name=config['s3']['region'],
         aws_access_key_id=config['s3']['access_key'],
-        aws_secret_access_key=config['s3']['secret_key'],
+        aws_secret_access_key=config['s3']['secret_key']
     )
 
     path = config['data']['path'].format(pwd='', project=project['name'], hash=commit['hash'])
     path = os.path.basename(path)
 
-    file = s3.Object(config['s3']['bucket'], path)
-    file.put(Body=json.dumps(metrics), ACL='public-read')
+    client.put_object(
+        Body=json.dumps(metrics),
+        Key=path,
+        ACL='public-read',
+        Bucket=config['s3']['bucket'],
+    )
