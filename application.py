5fc7f0261fa6a15ebfa346e89d616afc0bf953a7
Matthias Mullie <git@mullie.eu>
2016-03-06 22:39:06 +0100

    Explicitly close DB connection

--- application.py
+++ application.py
@@ -16,26 +16,34 @@ def application(environ, start_response):
     uri = environ["REQUEST_URI"]
     container = Container(environ)
 
-    # load existing cookies
-    cookies = http.cookies.SimpleCookie()
-    cookies.load(container.environ.get("HTTP_COOKIE", ""))
+    try :
+        # load existing cookies
+        cookies = http.cookies.SimpleCookie()
+        cookies.load(container.environ.get("HTTP_COOKIE", ""))
 
-    # init session (but don't load session data yet)
-    session_id = cookies['session_id'].value if 'session_id' in cookies else None
-    max_age = container.config['session']['max_age']
-    session = models.sessions.Model(container.mysql, session_id, max_age)
+        # init session (but don't load session data yet)
+        session_id = cookies['session_id'].value if 'session_id' in cookies else None
+        max_age = container.config['session']['max_age']
+        session = models.sessions.Model(container.mysql, session_id, max_age)
 
-    # figure out the right controller & let it do all the pre-processing it has to
-    # (e.g. validate input)
-    controller = controllers.route(uri, cookies, session, container)
+        # figure out the right controller & let it do all the pre-processing it has to
+        # (e.g. validate input)
+        controller = controllers.route(uri, cookies, session, container)
 
-    headers = controller.headers()
-    body = controller.render().encode('utf-8')
+        headers = controller.headers()
+        body = controller.render().encode('utf-8')
 
-    # fetch new (changed) cookies & write them
-    cookies = [("Set-Cookie", morsel.OutputString()) for morsel in controller.cookie_set.values()]
-    # save session changes (or extend session timeout)
-    session.close()
+        # fetch new (changed) cookies & write them
+        cookies = [("Set-Cookie", morsel.OutputString()) for morsel in controller.cookie_set.values()]
+        # save session changes (or extend session timeout)
+        session.close()
+
+        container.mysql.close()
+    except Exception as exception:
+        # make sure to terminate mysql connection, also in case of request failure
+        container.mysql.close()
+
+        raise exception
 
     start_response(controller.status, headers + cookies + [('Content-Length', str(len(body)))])
     return [body]
