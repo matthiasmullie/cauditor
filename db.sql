977a9f3f9bac1c90b9c57f62c473deb7e18e7e48
Matthias Mullie <git@mullie.eu>
2016-03-07 08:45:22 +0100

    Use unsigned ints & bigint for npath

--- db.sql
+++ db.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `projects` (
   `name` varchar(255) NOT NULL, -- vendor/project
   `git` blob NOT NULL, -- clone url
-  `github_id` int(11) DEFAULT NULL, -- repo id
-  `github_hook` int(11) DEFAULT NULL, -- hook id
+  `github_id` INT UNSIGNED DEFAULT NULL, -- repo id
+  `github_hook` INT UNSIGNED DEFAULT NULL, -- hook id
   PRIMARY KEY (`name`),
   UNIQUE KEY `idx_git` (`github_id`)
 ) DEFAULT CHARSET=utf8;
@@ -10,26 +10,26 @@ CREATE TABLE IF NOT EXISTS `projects` (
 CREATE TABLE IF NOT EXISTS `commits` (
   `project` varchar(255) NOT NULL, -- vendor/project
   `branch` varchar(255) NOT NULL,
-  `commit_id` int(11) NOT NULL, -- FK, with commit_details.id
+  `commit_id` INT UNSIGNED NOT NULL, -- FK, with commit_details.id
   PRIMARY KEY (`project`,`branch`,`commit_id`)
 ) DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `commit_details` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `hash` binary(40) NOT NULL, -- commit sha
   `previous` binary(40) DEFAULT NULL, -- previous commit sha
   `author` varchar(255) NOT NULL, -- author email
   `timestamp` datetime NOT NULL, -- commit date
   -- project-wide metrics as of this commit
-  `loc` int(11) NOT NULL,
-  `noc` int(11) NOT NULL,
-  `nom` int(11) NOT NULL,
-  `ca` int(11) NOT NULL,
-  `ce` int(11) NOT NULL,
+  `loc` INT UNSIGNED NOT NULL,
+  `noc` INT UNSIGNED NOT NULL,
+  `nom` INT UNSIGNED NOT NULL,
+  `ca` INT UNSIGNED NOT NULL,
+  `ce` INT UNSIGNED NOT NULL,
   `i` decimal(11, 2) NOT NULL,
-  `dit` int(11) NOT NULL,
-  `ccn` int(11) NOT NULL,
-  `npath` int(11) NOT NULL,
+  `dit` INT UNSIGNED NOT NULL,
+  `ccn` INT UNSIGNED NOT NULL,
+  `npath` BIGINT UNSIGNED NOT NULL,
   `he` decimal(11, 2) NOT NULL,
   `hi` decimal(11, 2) NOT NULL,
   `mi` decimal(11, 2) NOT NULL,
