7e94cba93802b19c7a2e474a9b9b8bbb2aedc008
Matthias Mullie <git@mullie.eu>
2016-03-16 13:36:50 +0100

    Don't catch exceptions to test if logged in

--- cauditor/controllers/api/put_settings.py
+++ cauditor/controllers/api/put_settings.py
@@ -7,21 +7,24 @@ class Controller(fallback.Controller):
     template = ""
 
     def headers(self):
-        model = models.settings.Model(self.container.mysql)
-
-        try:
+        if 'id' in self.user:
+            model = models.settings.Model(self.container.mysql)
             for key in self.data:
                 model.store({
                     'user': self.user['id'],
                     'key': key,
                     'value': self.data[key],
                 })
-        except Exception:
+        else:
+            # not logged in
             self.status = "401 Unauthorized"
 
         return super(Controller, self).headers()
 
     def render(self, template="container.html"):
+        if 'id' not in self.user:
+            return super(Controller, self).render(template)
+
         model = models.settings.Model(self.container.mysql)
         settings = model.select(user=self.user['id'])
 
