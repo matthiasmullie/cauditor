f5481947a817feb74871564f604ddc4abfc0d313
Matthias Mullie <git@mullie.eu>
2016-03-17 21:49:26 +0100

    Load previous commits

--- cauditor/controllers/api/get_commits.py
+++ cauditor/controllers/api/get_commits.py
@@ -9,5 +9,5 @@ class Controller(fallback.Controller):
 
     def get_commits(self):
         model = models.commits.Model(self.container.mysql)
-        imported = model.select(project=self.route['project'], branch=self.route['branch'])
+        imported = model.select(project=self.route['project'], branch=self.route['branch'], options=["ORDER BY timestamp DESC"])
         return [commit for commit in imported]
