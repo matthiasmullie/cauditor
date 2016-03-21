79016efe9d633e2385c3758c06683cdcd6a894c9
Matthias Mullie <git@mullie.eu>
2016-03-21 09:42:34 +0100

    Make import cron locate 64b packages

--- import_unlinked.py
+++ import_unlinked.py
@@ -5,6 +5,7 @@ import os
 
 # add path where packages are located in ElasticBeanstalk
 sys.path.append('/opt/python/run/venv/lib/python3.4/site-packages/')
+sys.path.append('/opt/python/run/venv/lib64/python3.4/site-packages/')
 
 # add current directory to ensure these modules can be imported
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
