b9a7c4548181fb60b773e31c0eba6632d2fe37fe
Matthias Mullie <git@mullie.eu>
2016-04-07 11:14:03 +0200

    Get rid of yaml leak

--- cauditor/container.py
+++ cauditor/container.py
@@ -19,15 +19,19 @@ class Container:
     @property
     def config(self):
         if self._config is None:
-            # parse environment variables into config.yaml, in $VARIABLE format
-            # @see http://stackoverflow.com/questions/26712003/pyyaml-parsing-of-the-environment-variable-in-the-yaml-configuration-file
-            def pathex_constructor(loader, node):
-                return os.environ[node.value[1:]]
-            pattern = re.compile(r'\$([0-9a-z_]+)', re.IGNORECASE)
-            yaml.add_implicit_resolver('tag', pattern)
-            yaml.add_constructor('tag', pathex_constructor)
+            # only add env vars resolver once!
+            if not 'env' in yaml.Loader.yaml_constructors:
+                # parse environment variables into config.yaml, in $VARIABLE format
+                # @see http://stackoverflow.com/questions/26712003/pyyaml-parsing-of-the-environment-variable-in-the-yaml-configuration-file
+                def pathex_constructor(loader, node):
+                    return os.environ[node.value[1:]]
+
+                pattern = re.compile(r'\$([0-9a-z_]+)', re.IGNORECASE)
+                yaml.add_implicit_resolver('env', pattern)
+                yaml.add_constructor('env', pathex_constructor)
 
             path = os.path.dirname(os.path.abspath(__file__)) + '/config.yaml'
+
             with open(path, 'r', encoding='utf-8') as stream:
                 self._config = yaml.load(stream)
 
